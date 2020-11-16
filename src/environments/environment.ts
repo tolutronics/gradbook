0806b35c93e9ce49fbc85946f397f8ee74b45f0b
tolutronics <toluxy100@gmail.com>
2020-11-16 10:32:51 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAsm63Sw1yN72CbyqCU8G3TWZ_HrFEp05o",
-  authDomain: "education-001.firebaseapp.com",
-  databaseURL: "https://education-001.firebaseio.com",
-  projectId: "education-001",
-  storageBucket: "education-001.appspot.com",
-  messagingSenderId: "97121885235",
-  appId: "1:97121885235:web:09241179679d796c545746",
-  measurementId: "G-18ZL9GYDZE"
+
   }
 };
 
